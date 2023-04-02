9979973fb366cfe844e71eab3450b5c152f87c15
gacurl <greg.a.curl@gmail.com>
2023-04-02 17:06:43 -0400

    solve q2 - make it a string

--- hangman.rb
+++ hangman.rb
@@ -2,15 +2,20 @@
 
 # Example: hangman("bob",["b"]) should evaluate to "b_b" Example: hangman("alphabet",["a","h"]) should return "a__ha___"
 def hangman (word, guesses)
+#   separate each letter IOT compare to guesses
+  array = []
   characters = word.split("")
-  
+#   Map transforms array from values returned from the block
+#   https://ruby-doc.org/core-2.7.0/Array.html#method-i-map
   characters.map do |char|
-    if guesses.include?(char)
-      char
+    if guesses.include?(char) #if char is included in array of guesses
+      array << char #push that Char into array
     else 
-      "-"
+      array << "_" #or push an underscore
     end
   end
+  array.join("") #make it a string IAW lesson
 end
 
-print hangman("bob", ["b"])
+p hangman("bob",["b"])
+p hangman("alphabet",["a","h"])
